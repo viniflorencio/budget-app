63777a8e07ba2325d85c715f52efcbef4756458a
viniflorencio <viniflorenciomid@gmail.com>
2022-05-14 02:25:39 -0300

    Feat basic html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
+import { Stack } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  return <Container>
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">
+            Add Budget
+          </Button>
+          <Button variant="outline-primary">
+            Add Expense
+          </Button>
+        </Stack>
+        <div style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
 
-      </header>
-    </div>
-  );
+        </div>
+
+  </Container>
 }
 
 export default App;
